781f06db79b2879cf741677c3c49edc0c56a6a3d
erdem <137915983+erdemonal11@users.noreply.github.com>
2025-07-04 19:05:36 +0000

    Ajout de plusieurs images de projet dans le répertoire public

--- app/page.tsx
+++ app/page.tsx
@@ -11,39 +11,151 @@ import { Dialog, DialogContent } from "@/components/ui/dialog"
 const images = [
 	{
 		id: 1,
-		src: "/placeholder.svg?height=600&width=800",
-		alt: "Projet de maçonnerie 1",
-		description: "Description du projet 1",
+		src: "/1.jpg"
 	},
 	{
 		id: 2,
-		src: "/placeholder.svg?height=600&width=800",
-		alt: "Projet de maçonnerie 2",
-		description: "Description du projet 2",
+		src: "/2.jpg",
 	},
 	{
 		id: 3,
-		src: "/placeholder.svg?height=600&width=800",
-		alt: "Projet de maçonnerie 3",
-		description: "Description du projet 3",
+		src: "/3.jpg",
 	},
 	{
 		id: 4,
-		src: "/placeholder.svg?height=600&width=800",
-		alt: "Projet de maçonnerie 4",
-		description: "Description du projet 4",
+		src: "/4.jpg",
 	},
 	{
 		id: 5,
-		src: "/placeholder.svg?height=600&width=800",
-		alt: "Projet de maçonnerie 5",
-		description: "Description du projet 5",
+		src: "/5.jpg",
 	},
 	{
 		id: 6,
-		src: "/placeholder.svg?height=600&width=800",
-		alt: "Projet de maçonnerie 6",
-		description: "Description du projet 6",
+		src: "/6.jpg",
+	},
+	{
+		id: 7,
+		src: "/7.jpg",
+	},
+	{
+		id: 8,
+		src: "/8.jpg",
+	},
+	{
+		id: 9,
+		src: "/9.jpg",
+	},
+	{
+		id: 10,
+		src: "/10.jpg",
+	},
+	{
+		id: 11,
+		src: "/11.jpg",
+	},
+	{
+		id: 12,
+		src: "/12.jpg",
+	},
+	{
+		id: 13,
+		src: "/13.jpg",
+	},
+	{
+		id: 14,
+		src: "/14.jpg",
+	},
+	{
+		id: 15,
+		src: "/15.jpg",
+	},
+	{
+		id: 16,
+		src: "/16.jpg",
+	},
+	{
+		id: 17,
+		src: "/17.jpg",
+	},
+	{
+		id: 18,
+		src: "/18.jpg",
+	},
+	{
+		id: 19,			
+		src: "/19.jpg",
+	},
+	{
+		id: 20,
+		src: "/20.jpg",
+	},
+	{
+		id: 21,
+		src: "/21.jpg",
+	},
+	{
+		id: 22,
+		src: "/22.jpg",
+	},
+	{
+		id: 23,
+		src: "/23.jpg",
+	},				
+	{
+		id: 24,
+		src: "/24.jpg",
+	},
+	{
+		id: 25,
+		src: "/25.jpg",
+	},
+	{
+		id: 26,
+		src: "/26.jpg",
+	},
+	{
+		id: 27,
+		src: "/27.jpg",
+	},
+	{
+		id: 28,
+		src: "/28.jpg",
+	},
+	{
+		id: 29,
+		src: "/29.jpg",
+	},
+	{
+		id: 30,
+		src: "/30.jpg",			
+	},
+	{
+		id: 31,
+		src: "/31.jpg",
+	},
+	{
+		id: 32,
+		src: "/32.jpg",
+	},
+	{
+		id: 33,
+		src: "/33.jpg",
+	},
+	{
+		id: 34,
+		src: "/34.jpg",
+	},
+	{
+		id: 35,
+		src: "/35.jpg",
+	},					
+	{
+		id: 36,
+		src: "/36.jpg",
+	},
+	{
+		id: 37,
+		src: "/37.jpg",
 	},
 ]
 
